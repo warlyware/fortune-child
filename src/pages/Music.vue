51145f429d002a4b786cd07daf56ed8e98777abc
Dan Ward <danward@gmail.com>
2022-02-03 01:07:06 -0800

    Add new single

--- src/pages/Music.vue
+++ src/pages/Music.vue
@@ -22,9 +22,29 @@
           src="../assets/far-cover.jpeg"
           alt="Far Album Cover"
         >
-        <div class="py-8">
-          Listen to the new single on Spotify
-        </div>
+      </a>
+    </div>
+    <div class="px-12 mx-auto space-y-4 text-lg text-left max-w-prose">
+      <div class="h-64 my-12 md:h-96 w-100">
+        <iframe
+          id="yt-player"
+          class="bg-gray-800"
+          type="text/html"
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/vIEK6prirow?autoplay=1&origin=https://fortunechildmusic.com"
+          frameborder="0"
+        />
+      </div>
+      <a
+        href="https://distrokid.com/hyperfollow/fortunechild/slow-down-3"
+        target="_blank"
+        class="block text-2xl text-center uppercase"
+      >
+        <img
+          src="../assets/slow-down-cover.jpeg"
+          alt="Slow Down Cover"
+        >
       </a>
     </div>
   </div>
